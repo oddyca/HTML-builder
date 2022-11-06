866bfca39c0fa03691898d8ab5975023359bcdf7
Siarhei Prakapovich <prokser27@icloud.com>
2022-11-07 01:17:55 +0300

    feat: write a file reading script

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,2 +1,14 @@
+const {stdout} = process;
+
 const fs = require('fs');
 const path = require('path');
+
+fs.readFile(
+  path.join(__dirname, 'text.txt'),
+  'utf-8',
+  (err, data) => {
+    if (err) throw err;
+    stdout.write(data);
+    process.exit();
+  }
+)
