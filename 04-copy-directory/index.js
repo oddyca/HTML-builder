8da076f4d1ac6e90e6a2247fbec7b9f28aaeb44f
Siarhei Prakapovich <prokser27@icloud.com>
2022-11-08 02:30:09 +0300

    feat: write the folder copying script

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+const fsPromise = require('fs/promises');
+//const fs = require('fs');
+const path = require('path');
 
+async function start(){
+  try {
+    const createDir = await fsPromise.mkdir(__dirname + '/files-copy/', { recursive: true });
+    console.log(`created ${createDir}`);
+    const files = await fsPromise.readdir(path.join(__dirname, 'files'));
+
+    files.forEach(async file => {
+      const originPath = path.join(__dirname, 'files', path.basename(file));
+      const targetPath = path.join(__dirname, 'files-copy', path.basename(file));
+      await fsPromise.copyFile(originPath, targetPath);
+    })
+  } catch (err) {
+    console.log(__dirname + '/files-copy/')
+    console.error(err.message);
+  }
+}
+
+start();
