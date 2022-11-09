07b9c01492f0387f5ad1cc80a992f2e6d1a6e85a
Siarhei Prakapovich <prokser27@icloud.com>
2022-11-09 18:32:49 +0300

    feat: add functionality of deleting a file if it is not present in the original folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,22 +1,40 @@
 const fsPromise = require('fs/promises');
-//const fs = require('fs');
 const path = require('path');
 
 async function start(){
   try {
-    const createDir = await fsPromise.mkdir(__dirname + '/files-copy/', { recursive: true });
-    console.log(`created ${createDir}`);
-    const files = await fsPromise.readdir(path.join(__dirname, 'files'));
+    const isCopied = await fsPromise.readdir(path.join(__dirname));
+    console.log(isCopied)
 
-    files.forEach(async file => {
-      const originPath = path.join(__dirname, 'files', path.basename(file));
-      const targetPath = path.join(__dirname, 'files-copy', path.basename(file));
-      await fsPromise.copyFile(originPath, targetPath);
-    })
+    if (!isCopied.includes("files-copy")) {
+      await fsPromise.mkdir(__dirname + '/files-copy/', { recursive: true });
+      const files = await fsPromise.readdir(path.join(__dirname, 'files'));
+
+      files.forEach(async file => {
+        const originPath = path.join(__dirname, 'files', path.basename(file));
+        const targetPath = path.join(__dirname, 'files-copy', path.basename(file));
+        await fsPromise.copyFile(originPath, targetPath);
+      })
+    } else {
+      const files = await fsPromise.readdir(path.join(__dirname, 'files'));
+      const filesCopy = await fsPromise.readdir(path.join(__dirname, 'files-copy'));
+
+      filesCopy.forEach(async fileCopy => {
+        if (!files.includes(fileCopy)) {
+          await fsPromise.unlink(path.join(__dirname, 'files-copy', fileCopy))
+        }
+      });
+
+      files.forEach(async file => {
+        const originPath = path.join(__dirname, 'files', path.basename(file));
+        const targetPath = path.join(__dirname, 'files-copy', path.basename(file));
+        await fsPromise.copyFile(originPath, targetPath);
+      })
+    }
   } catch (err) {
-    console.log(__dirname + '/files-copy/')
     console.error(err.message);
   }
+  
 }
 
 start();
