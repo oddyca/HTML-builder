35d0bf06562ba6a1b54e6247529a07490a2255b9
Siarhei Prakapovich <prokser27@icloud.com>
2022-11-10 19:12:09 +0300

    feat: add check whether the content in the folder is a file or a folder in 03-files-in-folder task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -7,8 +7,10 @@ fs.readdir(path.join(__dirname, 'secret-folder'), (err, files) => {
     console.log(err);
   else {
     files.forEach(async file => {
-      let size = await fsPromises.stat(path.join(__dirname, 'secret-folder', file));
-      console.log(path.parse(file).name, "—", path.extname(file).slice(1), "—", size.size + 'kb');
+      let info = await fsPromises.stat(path.join(__dirname, 'secret-folder', file));
+      if (info.isFile()){
+        console.log(path.parse(file).name, "—", path.extname(file).slice(1), "—", info.size + 'kb');
+      }
     })
   }
 })
